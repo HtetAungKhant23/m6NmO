0e7ba7bd5b3667d9c224cd2714a0e2a7d1b7ca7b
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-28 23:11:53 +0630

    publish: m6numo package for my homies

---
+++ index.js
@@ -0,0 +1,117 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { sharpMan } = require("sharp");
+
+/**
+ * @class
+ * @classdesc Represents a class for uploading files to an S3 bucket.
+ */
+class m6NmO {
+  /**
+   * @param {string} bucketName - The name of the S3 bucket.
+   * @param {string} region - The AWS region of the S3 bucket.
+   * @param {string} accessKeyId - The AWS access key ID for authentication.
+   * @param {string} secretAccessKey - The AWS secret access key for authentication.
+   */
+  bucketName;
+  constructor(bucketName, region, accessKeyId, secretAccessKey) {
+    if (!bucketName || !region || !accessKeyId || !secretAccessKey) {
+      throw new Error("All parameters are required.");
+    }
+    this.bucketName = bucketName;
+    this.s3Client = new S3Client({
+      region: region,
+      credentials: {
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey,
+      },
+    });
+  }
+
+  /**
+   * Uploads a file to the S3 bucket.
+   *
+   * @param {Buffer} fileBuffer - The content of the file to be uploaded.
+   * @param {string} fileName - The name of the file in the S3 bucket.
+   * @param {string} mimetype - The MIME type of the file.
+   * @returns {Promise<Object>} - A promise that resolves to the result of the S3 upload operation.
+   */
+  async uploadFile(fileBuffer, fileName, mimetype) {
+    const buffer = Buffer.from(fileBuffer.data);
+    const img = await sharpMan(buffer)
+      .resize({ height: 720, width: 1280, fit: "contain" })
+      .toBuffer();
+    const uploadParams = {
+      Bucket: this.bucketName,
+      Body: img,
+      Key: fileName,
+      ContentType: mimetype,
+    };
+
+    return this.s3Client.send(new PutObjectCommand(uploadParams));
+  }
+
+  /**
+   * Uploads an Excel file to the S3 bucket.
+   *
+   * @param {any} fileBuffer - The content of the Excel file to be uploaded.
+   * @param {string} fileName - The name of the file in the S3 bucket.
+   * @returns {Promise<Object>} - A promise that resolves to the result of the S3 upload operation.
+   */
+  async uploadExcel(fileBuffer, fileName) {
+    const uploadParams = {
+      Bucket: this.bucketName,
+      Key: fileName,
+      Body: fileBuffer,
+    };
+    try {
+      const result = await this.s3Client.send(
+        new PutObjectCommand(uploadParams)
+      );
+      return result;
+    } catch (error) {
+      console.error("Error uploading Excel file:", error);
+      throw error; // Re-throw the error for handling at a higher level if needed
+    }
+  }
+
+  /**
+   * Deletes a file from the S3 bucket.
+   *
+   * @param {string} fileName - The name of the file in the S3 bucket to be deleted.
+   * @returns {Promise<Object>} - A promise that resolves to the result of the S3 delete operation.
+   */
+  deleteFile(fileName) {
+    const deleteParams = {
+      Bucket: this.bucketName,
+      Key: fileName,
+    };
+
+    return this.s3Client.send(new DeleteObjectCommand(deleteParams));
+  }
+
+  /**
+   * Gets a signed URL for downloading an object from the S3 bucket.
+   *
+   * @param {string} key - The key (filename) of the object in the S3 bucket.
+   * @returns {Promise<string>} - A promise that resolves to the signed URL for the object.
+   */
+  async getObjectSignedUrl(key) {
+    const params = {
+      Bucket: this.bucketName,
+      Key: key,
+    };
+    const command = new GetObjectCommand(params);
+    const seconds = 60; // Adjust the expiration time as needed
+    try {
+      const url = await getSignedUrl(this.s3Client, command, {
+        expiresIn: seconds,
+      });
+      return url;
+    } catch (error) {
+      console.error("Error generating signed URL:", error);
+      throw error; // Re-throw the error for handling at a higher level if needed
+    }
+  }
+}
+
+module.exports = m6NmO;
---
+++ readMe.md
@@ -0,0 +1,70 @@
+# m6numo
+
+**m6numo** is a JavaScript package providing a class for simplified interaction with Amazon S3. This package allows you to upload, delete, and retrieve files from an S3 bucket.
+
+## Installation
+
+Install the package using npm:
+
+```bash
+npm install m6numo
+```
+
+# Usage
+
+## Import
+
+```javascript
+import { m6NmO } from "m6nmo";
+```
+
+## Create an instance
+
+```javascript
+const myUploader = new m6numo(
+  "your-bucket-name",
+  "your-region",
+  "your-access-key-id",
+  "your-secret-access-key"
+);
+```
+
+## Upload a file
+
+```javascript
+const fileBuffer = /* Buffer containing your file */;
+const fileName = "example.jpg";
+const mimetype = "image/jpeg";
+
+myUploader.uploadFile(fileBuffer, fileName, mimetype)
+  .then(result => console.log("File uploaded successfully:", result))
+  .catch(error => console.error("Error uploading file:", error));
+
+```
+
+## Delete a file
+
+```javascript
+const fileNameToDelete = "example.jpg";
+
+myUploader
+  .deleteFile(fileNameToDelete)
+  .then((result) => console.log("File deleted successfully:", result))
+  .catch((error) => console.error("Error deleting file:", error));
+```
+
+## Upload a excel file
+
+```javascript
+const fileBuffer = /* Buffer containing your file */;
+const fileNameToUpload = "example.xlsx";
+
+myUploader
+  .uploadExcel(fileBuffer, fileNameToUpload)
+  .then((result) => console.log("File uploaded successfully:", result))
+  .catch((error) => console.error("Error uploading file:", error));
+```
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE.md](LICENSE.md) file for details.
